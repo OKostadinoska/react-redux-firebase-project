839ffaf6aadc5345141e41cd6defe726c0d49074
Olivera Kostadinoska <kostadinoskaolivera@outlook.com>
2022-06-14 22:03:33 +0200

    Implemented Dashboard component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import Dashboard from './components/dashboard/Dashboard';
 import Navbar from './components/layout/Navbar';
 import SignedInLinks from './components/layout/SignedInLinks';
 import SignedOutLinks from './components/layout/SignedOutLinks';
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <SignedInLinks />
         <SignedOutLinks />
+        <Dashboard />
       </div>
     </BrowserRouter>
   );
---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <div row>
+        <div column></div>
+        <div column></div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
